db024d7592a597646d7b0a9d3e97ce67472042ae
Nirav Prajapati <web.niravprajapati@gmail.com>
2020-08-09 17:49:08 +0530

    completed upto 18 - local-storage-custom-hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from 'routes'
+import Topbar from 'components/topbar'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Topbar />
+        <Routes />
+      </Router>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
